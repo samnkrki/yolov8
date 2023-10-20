e2980b6ed8b2237b8331691d07e41324e4f6a484
samnkrki <samn_krki@live.com>
2023-10-20 17:57:40 +0545

    files added in main

--- train_potato.py
+++ train_potato.py
@@ -1,19 +1,24 @@
 from ultralytics import YOLO
 
-# Create a new YOLO model from scratch
-model = YOLO('yolov8n.yaml')
+if __name__ == '__main__':
+    augment_args = {}
+    training_args = {
+        'epochs': 100,
+        'patience': 20,
+        'optimizer': 'adam',
+    }
 
-# Load a pretrained YOLO model (recommended for training)
-model = YOLO('yolov8n.pt')
+    # Load a pretrained YOLO model (recommended for training)
+    model = YOLO('yolov8n.pt')
 
-# Train the model using the 'coco128.yaml' dataset for 3 epochs
-results = model.train(data='data/potato.yaml', epochs=100, patience=20, batch=4, imgsz=640, project="potato", name="potato-", classes=[0], augment=True, visualize=True, device=0)
+    # Train the model using the 'coco128.yaml' dataset for 3 epochs
+    results = model.train(data='data/potato.yaml', epochs=100, patience=20, batch=4, imgsz=640, project="potato", name="potato-", classes=[0], augment=True, visualize=True, device=0)
 
-# Evaluate the model's performance on the validation set
-results = model.val()
+    # Evaluate the model's performance on the validation set
+    results = model.val()
 
-# Perform object detection on an image using the model
-results = model('https://ultralytics.com/images/bus.jpg')
+    # Perform object detection on an image using the model
+    results = model('https://ultralytics.com/images/bus.jpg')
 
-# Export the model to ONNX format
-success = model.export(format='onnx')
+    # Export the model to ONNX format
+    success = model.export(format='onnx')
