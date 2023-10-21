66dbdeaaa6a5bd82ec98a5f7dc9b25e94323b0a1
samnkrki <samn_krki@live.com>
2023-10-21 13:06:23 +0545

    device set to cuda

--- train_potato.py
+++ train_potato.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     model = YOLO('yolov8n.pt')
 
     # Train the model using the 'coco128.yaml' dataset for 3 epochs
-    results = model.tune(data=dataset_path, epochs=100, patience=20, batch=4, imgsz=640, project="potato", name="potato-", classes=[0], augment=True, visualize=True, device='cpu', optimizer="Adam", lr0=0.001, use_ray=True)
+    results = model.tune(data=dataset_path, epochs=100, patience=20, batch=4, imgsz=640, project="potato", name="potato-", classes=[0], augment=True, visualize=True, device=0, optimizer="Adam", lr0=0.001, use_ray=True)
 
     # Evaluate the model's performance on the validation set
     results = model.val()
