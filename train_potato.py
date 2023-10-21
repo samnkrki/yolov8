53989dd93fcbae768604e59701c65b0f75362a41
samnkrki <samn_krki@live.com>
2023-10-21 13:03:52 +0545

    raytune dataset path must be absolute

--- train_potato.py
+++ train_potato.py
@@ -3,6 +3,7 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 if __name__ == '__main__':
+    dataset_path = '/Users/saminator/Documents/yolov8/data/potato.yaml'
     augment_args = {}
     training_args = {
         'epochs': 100,
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     model = YOLO('yolov8n.pt')
 
     # Train the model using the 'coco128.yaml' dataset for 3 epochs
-    results = model.tune(data='data/potato.yaml', epochs=100, patience=20, batch=4, imgsz=640, project="potato", name="potato-", classes=[0], augment=True, visualize=True, device=0, optimizer="Adam", lr0=0.001, use_ray=True, iterations=10)
+    results = model.tune(data=dataset_path, epochs=100, patience=20, batch=4, imgsz=640, project="potato", name="potato-", classes=[0], augment=True, visualize=True, device='cpu', optimizer="Adam", lr0=0.001, use_ray=True)
 
     # Evaluate the model's performance on the validation set
     results = model.val()
