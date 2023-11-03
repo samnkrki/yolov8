49ca29f2eaa04c9069c5427b940c294b36067f3a
samnkrki <samn_krki@live.com>
2023-11-03 18:40:20 +0545

    for windows, 50 datasets

--- train_potato.py
+++ train_potato.py
@@ -6,8 +6,8 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 if __name__ == '__main__':
     # dataset_path = '/Users/saminator/Documents/yolov8/data/potato.yaml'
-    # dataset_path = 'C:/Users/USER/Documents/samin/yolov8/data/potato.yaml'
-    dataset_path = '/home/wakanda/Documents/samin/yolov8/data/potato.yaml'
+    dataset_path = 'C:/Users/USER/Documents/samin/yolov8/data/potato.yaml'
+    # dataset_path = '/home/wakanda/Documents/samin/yolov8/data/potato.yaml'
     train_args = dict(data=dataset_path, epochs=100, batch=4, imgsz=640, project="potato", name="potato-", augment=True, visualize=True, device=0, optimizer="Adam")
     train_args['classes'] = [0]
     augment_args = dict()
@@ -43,8 +43,9 @@ if __name__ == '__main__':
     model = YOLO('yolov8n.pt')
 
     # Train the model using the 'coco128.yaml' dataset for 3 epochs
-    results = model.tune(**train_args, **augment_args, use_ray=True)
-
+    # results = model.tune(**train_args, **augment_args, suse_ray=True)
+    results = model.train(**train_args, **augment_args)
+    
     # Evaluate the model's performance on the validation set
     results = model.val()
 
