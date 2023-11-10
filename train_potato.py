290d2b17e97402584372d91a625d9819791865c2
samnkrki <samn_krki@live.com>
2023-11-10 07:32:23 +0545

    yolov5 hyperparameters high

--- train_potato.py
+++ train_potato.py
@@ -12,33 +12,33 @@ if __name__ == '__main__':
     train_args['data'] = dataset_path
     train_args['classes'] = [0]
     augment_args = dict()
-    augment_args['lr0']= 0.08591211666330036 # initial learning rate (i.e. SGD=1E-2, Adam=1E-3)
-    augment_args['lrf']= 0.678455581881708 # final learning rate (lr0 * lrf)
-    augment_args['momentum']= 0.8484255646838734 # SGD momentum/Adam beta1
-    augment_args['weight_decay']= 0.0008491207411328716 # optimizer weight decay 5e-4
-    augment_args['warmup_epochs']= 0.7338494867123063 # warmup epochs (fractions ok)
-    augment_args['warmup_momentum']= 0.38158103445764946 # warmup initial momentum
+    augment_args['lr0']= 0.001 # initial learning rate (i.e. SGD=1E-2, Adam=1E-3)
+    augment_args['lrf']= 0.1 # final learning rate (lr0 * lrf)
+    augment_args['momentum']= 0.937 # SGD momentum/Adam beta1
+    augment_args['weight_decay']= 0.0005 # optimizer weight decay 5e-4
+    augment_args['warmup_epochs']= 3.0 # warmup epochs (fractions ok)
+    augment_args['warmup_momentum']= 0.8 # warmup initial momentum
     # augment_args['warmup_bias_lr']= 0.1 # warmup initial bias lr
-    augment_args['box']= 0.043838900664838525 # box loss gain
-    augment_args['cls']= 3.978326462737403 # cls loss gain (scale with pixels)
+    augment_args['box']= 0.05 # box loss gain
+    augment_args['cls']= 0.03 # cls loss gain (scale with pixels)
     # # augment_args['dfl']= 0.56016 # dfl loss gain
     # # augment_args['pose']= 12.0 # pose loss gain
     # # augment_args['kobj']= 1.0 # keypoint obj loss gain
     # # augment_args['label_smoothing']= 0.0 # label smoothing (fraction)
     # augment_args['nbs']= 64 # nominal batch size
-    augment_args['hsv_h']= 4.2636002823248643e-05 # image HSV-Hue augmentation (fraction)
-    augment_args['hsv_s']= 0.5959737499224373 # image HSV-Saturation augmentation (fraction)
-    augment_args['hsv_v']= 0.11557579376895563 # image HSV-Value augmentation (fraction)
-    augment_args['degrees']= 18.24923038358967 # image rotation (+/- deg)
-    augment_args['translate']= 0.24858666563876924 # image translation (+/- fraction)
-    augment_args['scale']= 0.47681002138525624 # image scale (+/- gain)
-    augment_args['shear']= 8.382338793487836 # image shear (+/- deg)
-    augment_args['perspective']= 0.0001243827075651327 # image perspective (+/- fraction), range 0-0.001
-    augment_args['flipud']= 0.1862331297252371 # image flip up-down (probability)
-    augment_args['fliplr']= 0.28806696189630443 # image flip left-right (probability)
-    augment_args['mosaic']= 0.5805690772400773 # image mosaic (probability)
-    augment_args['mixup']= 0.619121254748873 # image mixup (probability)
-    augment_args['copy_paste']= 0.8733866793454359 # segment copy-paste (probability)
+    augment_args['hsv_h']= 0.015 # image HSV-Hue augmentation (fraction)
+    augment_args['hsv_s']= 0.7 # image HSV-Saturation augmentation (fraction)
+    augment_args['hsv_v']= 0.4 # image HSV-Value augmentation (fraction)
+    augment_args['degrees']= 0.0 # image rotation (+/- deg)
+    augment_args['translate']= 0.1 # image translation (+/- fraction)
+    augment_args['scale']= 0.9 # image scale (+/- gain)
+    augment_args['shear']= 0.0 # image shear (+/- deg)
+    augment_args['perspective']= 0.0 # image perspective (+/- fraction), range 0-0.001
+    augment_args['flipud']= 0.5 # image flip up-down (probability)
+    augment_args['fliplr']= 0.5 # image flip left-right (probability)
+    augment_args['mosaic']= 1.0 # image mosaic (probability)
+    augment_args['mixup']= 0.1 # image mixup (probability)
+    augment_args['copy_paste']= 0.1 # segment copy-paste (probability)
 
     # Load a pretrained YOLO model (recommended for training)
     model = YOLO('yolov8n.pt')
