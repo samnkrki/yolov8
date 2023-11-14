1b9009c022ea856706ba5cbc7e2c13b5468fc574
Samin Karki <samn_krki@live.com>
2023-11-14 19:20:36 +0545

    inference dat updated

--- sample-inference.py
+++ sample-inference.py
@@ -5,14 +5,14 @@ import shutil
 # Define output folder
 output_folder = 'inference'
 
-def save_inference(input_folder, model):
+def save_inference(input_folder, model, project, name):
     # Loop over all image files in input directory
     for filename in os.listdir(input_folder):
         if filename.endswith('.jpg') or filename.endswith('.png'):
             # Load image and run inference
             img_path = os.path.join(input_folder, filename)
             img = Image.open(img_path)
-            results = model(img, save_txt=True)
+            results = model(img, save_txt=True, project=project,exist_ok=True, name=name)
             print(results)
             # Save detection output to runs/detect/ folder
             # results.save()
@@ -23,8 +23,9 @@ def save_inference(input_folder, model):
 
 if __name__ == '__main__':
     project = 'potato'
-    name="potato-28"
+    folder_name='val_2560'
+    name="potato-31"
     path = f'./{project}/{name}/weights/best.pt'
-    dataset_path = 'c:/Users/USER/Documents/datasets/potato_many/train_2560/images'
+    dataset_path = f'c:/Users/USER/Documents/datasets/potato_many/{folder_name}/images'
     model = YOLO(path)
-    save_inference(dataset_path, model)
+    save_inference(dataset_path, model, project, name+'/'+folder_name)
